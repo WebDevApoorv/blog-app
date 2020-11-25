5f5811cdf1b3e61165f2eb5a33358d98e035553f
WebDevApoorv <apoorv.dwivedi96@gmail.com>
2020-11-25 13:55:14 +0530

    First version

---
+++ blog/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+from django.views.generic import ListView
+
+from .models import Post
+
+class BlogPostView(ListView):
+    model = Post
+    template_name='home.html'
+
+
